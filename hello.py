59c75ca7f14911bdc1187fdb62843085b89c7f93
thxit <1815078972@qq.com>
2016-11-14 17:19:12 +0800

    set cookie

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import make_response
+
 app = Flask(__name__)
 
+# how to set cookie
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer','42')
+    return response
