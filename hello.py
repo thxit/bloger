4e563a527d67689e5def7d35cf29d2d18f836b3a
thxit <1815078972@qq.com>
2016-11-15 22:19:26 +0800

    bootstrap and localization time

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_moment import Moment
+from datetime import datetime
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+moment = Moment(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
