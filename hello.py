ef8852420abe0cb3ee3f164cc98886de18de58c5
thxit <1815078972@qq.com>
2016-11-17 20:00:39 +0800

    flash message

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+#coding=utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+from flask import Flask, render_template, url_for, session, redirect, flash
 from flask_script import Manager
 from flask_moment import Moment
 from datetime import datetime
@@ -14,19 +19,24 @@ bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'hard to guess'
 
+
 class NameForm(Form):
-    name = StringField(u'你的名字是?',validators=[Required()])
-    submit = SubmitField(u'确定')
+    name = StringField('你的名字是?',validators=[Required()])
+    submit = SubmitField('确定')
 
 
 @app.route('/', methods = ['GET','POST'])
 def index():
-    name = None
     form = NameForm()
+
     if form.validate_on_submit():
-        name = form.data.name
-        form.data.name = ''
-    return render_template('index.html', form = form ,name = name)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('你已更改了名字')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
 
 @app.route('/user/<name>')
 def user(name):
