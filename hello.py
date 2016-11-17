ee95dbedf60b5b8afa3f83d0535b48c1926d287c
thxit <1815078972@qq.com>
2016-11-17 22:13:32 +0800

    operation of database

--- hello.py
+++ hello.py
@@ -4,43 +4,91 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 from flask import Flask, render_template, url_for, session, redirect, flash
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_moment import Moment
 from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+import os
 
 app = Flask(__name__)
 manager = Manager(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# 设置为True时，每次请求结束后自动提交数据库改动
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 app.config['SECRET_KEY'] = 'hard to guess'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
 class NameForm(Form):
-    name = StringField('你的名字是?',validators=[Required()])
+    name = StringField('你的名字是?',validators=[DataRequired()])
     submit = SubmitField('确定')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # 一个角色可以属于多个用户
+    # 加入lazy参数以便更好添加过滤器
+    user = db.relationship('User',backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role % r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    # 每个用户只能有一个角色
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User % r>' % self.username
 
 
 @app.route('/', methods = ['GET','POST'])
 def index():
     form = NameForm()
-
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('你已更改了名字')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html',
+        form=form, name=session.get('name'),
+        known=session.get('known', False))
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+def make_shell_context():    # 将对象添加到导入列表中并且自动导入
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
